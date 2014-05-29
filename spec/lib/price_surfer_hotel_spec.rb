db04b0394b7ac11eae6fc0aac6778a089b7e7cb5
Guido Collino <collinoguido@gmail.com>
2014-05-29 11:44:40 -0300

    test NameError

--- spec/lib/price_surfer_hotel_spec.rb
+++ spec/lib/price_surfer_hotel_spec.rb
@@ -6,4 +6,10 @@ describe PriceSurferHotel do
     element_data.should_not be_nil
     element_data == 'hola'
   end
+
+
+  it "no existe chau" do
+    expect{PriceSurferHotel.chau}.to raise_error(NameError)
+  end
+
 end
