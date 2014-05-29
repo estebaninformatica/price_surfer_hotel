2bd5b453db9955e967081c287f75aeea513b1e82
Fuhrmann Esteban <esteban.informatica@gmail.com>
2014-05-29 13:55:15 -0300

    del

--- lib/price_surfer_hotel.rb
+++ lib/price_surfer_hotel.rb
@@ -1,8 +1,38 @@
 require "price_surfer_hotel/version"
+require 'wadl'
 
 module PriceSurferHotel
   # Your code goes here...
   def self.hola
   	"hola2"
   end
+
+  def self.chau
+  end
+
+ #  def self.wald
+	# 	delicious = WADL::Application.from_wadl(open("lib/delicious.wadl")).v1
+	# 	delicious = delicious.with_basic_auth('username', 'password' )
+
+	# 	query_args = { :url => 'https://wadl.dev.java.net/',
+	# 	             :description => 'WADL homepage',
+	# 	             :extended => 'Posted with Ruby WADL client' }
+	# 	begin
+	# 	delicious.posts.add.get(:query => query_args)
+	# 	rescue WADL::Faults::AuthorizationRequired
+	# 	puts "Invalid authentication information!"
+	# 	end
+
+	# 	#delicious.posts.add.addPost(:query => query_args)
+	# 	#delicious.posts.addPost(:query => query_args)
+
+	# 	begin
+	# 	delicious.posts.recent.get.representation.each_element("post") do |e|
+	# 	  puts "#{e.attributes['description']}: #{e.attributes['href']}"
+	# 	end
+	# 	rescue WADL::Faults::AuthorizationRequired
+	# 	puts "Invalid authentication information!"
+	# 	end
+	# end
+
 end
