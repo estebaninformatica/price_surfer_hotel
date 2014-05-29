8f11e6d8c331306abd999f07708b0ddbcae798d4
Fuhrmann Esteban <esteban.informatica@gmail.com>
2014-05-29 15:37:33 -0300

    delicius

--- spec/lib/price_surfer_hotel_spec.rb
+++ spec/lib/price_surfer_hotel_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
-
+require 'wadl'
 describe PriceSurferHotel do
+
   it "hola" do
     element_data = PriceSurferHotel.hola
     p PriceSurferHotel.hola
@@ -13,4 +14,22 @@ describe PriceSurferHotel do
     expect{PriceSurferHotel.chau}.to raise_error(NameError)
   end
 
+  it "delicius post" do
+  	file = open(Dir.pwd + "/lib/delicious.wadl")
+
+  	
+  	delicious = WADL::Application.from_wadl(file).v1
+
+		delicious = delicious.with_basic_auth('estebanfuhrmann', 'holahola' )
+
+		query_args = { :url => 'https://wadl.dev.java.net/',
+		              :description => 'WADL homepage2',
+		              :extended => 'Posted with Ruby WADL client222' }
+		begin
+			delicious.posts.add.get(:query => query_args)
+		rescue WADL::Faults::AuthorizationRequired
+			puts "Invalid authentication information!"
+		end
+
+	end
 end
